d897aa8dc312ac539a77084f286babee445cc96f
JCarston <notsrac.jacob@gmail.com>
2024-01-12 09:15:55 -0700

    Adding additional unit tests

--- Python-Scripts/Unit-Testing/food_test.py
+++ Python-Scripts/Unit-Testing/food_test.py
@@ -1,5 +1,5 @@
 import unittest
-from food import eat
+from food import eat, nap
 class ActivityTests(unittest.TestCase):
     def test_eat_healthy(self): #Test One
         self.assertEqual(
@@ -11,5 +11,17 @@ class ActivityTests(unittest.TestCase):
             eat("pizza", is_healthy=False), #Behavior that is expected
             "I'm eating pizza, because YOLO!" #Expected Response
         )
+    def test_short_nap(self):
+        self.assertEqual(
+            nap(1),
+            "I'm feeling refreshed after my 1 hour nap"
+        )
+    def test_long_nap(self):
+        self.assertEqual(
+            nap(3),
+            "Ugh I overslepft. I didn't mean to nap so long"
+        )
+                
+        
 if __name__ == "__main__":
     unittest.main()
